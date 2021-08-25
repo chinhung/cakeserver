a55c6fa3ae10a6452330c8d6dbc6f6f5f2bc7ee7
chinhung <chinhung101@gmail.com>
2021-08-25 15:07:08 +0800

    Modify README

--- src/component/sidebar/CakeServerLogo.js
+++ src/component/sidebar/CakeServerLogo.js
@@ -2,7 +2,7 @@ function CakeServerLogo() {
     return (
         <div className="w3-bar-item w3-button w3-border-bottom w3-large">
             <h3>Cake Server</h3>
-            <h5 style={{ color: 'grey' }}>Makes your task as peices of cake!</h5>
+            <h5 style={{ color: 'grey' }}>Splits your task into peices of cake!</h5>
         </div>       
     );
 }
