32402ef3c4ae630f4352f5b347e735cf00ea824b
chinhung <chinhung101@gmail.com>
2021-08-25 15:27:55 +0800

    Add GitHub link in GitHub component

--- src/component/sidebar/GitHub.js
+++ src/component/sidebar/GitHub.js
@@ -1,6 +1,6 @@
 function GitHub() {
     return (
-        <div className="w3-bar-item w3-button"><i className="fa fa-github w3-margin-right"></i>GitHub</div>
+        <a href="https://github.com/chinhung/cakeserver" target="_blank" rel="noreferrer" className="w3-bar-item w3-button"><i className="fa fa-github w3-margin-right"></i>GitHub</a>
     );
 }
 
